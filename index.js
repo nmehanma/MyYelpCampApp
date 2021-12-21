5b814ad3cf3c774ca4be2c652c671dcf2139fee4
nmehanma <nmehanma@gmail.com>
2021-12-21 18:30:52 -0500

    create campground index

--- index.js
+++ index.js
@@ -23,11 +23,10 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({title: 'My Backyard', description: 'cheap camping!' }); 
-  await camp.save();
-  res.send(camp)
-});
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', {campgrounds})
+})
 
 app.listen(8080, () => {
   console.log("Listening on Port 8080");
