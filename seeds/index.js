23f7a9bde4af30a62385a4417a04f13a4fde32f9
nmehanma <nmehanma@gmail.com>
2021-12-21 23:02:39 -0500

    introducing unsplash api

--- seeds/index.js
+++ seeds/index.js
@@ -17,16 +17,35 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
+
+  const seedImg = async () => {
+    try {
+      const resp = await axios.get('https://api.unsplash.com/photos/random', {
+        params: {
+          client_id: 'ACCESS_KEY',
+          collections: 483251,
+        },
+      })
+      return resp.data.urls.small
+    } catch (err) {
+      console.error(err)
+    }
+  }
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus qui at fugit dolorem rem. Nulla aspernatur asperiores, fugit placeat velit omnis doloribus est quam similique harum praesentium iusto fugiat minus.",
+      price
     });
     await camp.save();
   }
 };
 
 seedDB().then(() => {
-  mongoose.connection.close()
+  mongoose.connection.close();
 });
