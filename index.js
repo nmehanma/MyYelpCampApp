6a8bee26e9069c8ab038ba3893b59469262676bf
nmehanma <nmehanma@gmail.com>
2021-12-21 19:39:25 -0500

    edit and update functionality introduced to app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+const methodOverride = require('method-override')
 
 mongoose.connect("mongodb://localhost:27017/myyelpcamp", {
   useNewURLParser: true,
@@ -18,9 +19,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true }))
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -31,17 +31,32 @@ app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", async(req, res) => {
+app.post("/campgrounds", async (req, res) => {
   const campground = new Campground(req.body.campground);
   await campground.save();
-  res.redirect(`/campgrounds/${campground._id}`)
+  console.log("New Campground created!");
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
- 
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/show", { campground });
 });
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params
+  const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
+
+
+
 
 app.listen(8080, () => {
   console.log("Listening on Port 8080");
