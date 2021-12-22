3646e2874175ad36673dd2c53380428987319128
nmehanma <nmehanma@gmail.com>
2021-12-21 19:51:10 -0500

    introduced delete functionality

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/myyelpcamp", {
   useNewURLParser: true,
@@ -20,7 +20,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -48,15 +48,20 @@ app.get("/campgrounds/:id/edit", async (req, res) => {
   res.render("campgrounds/edit", { campground });
 });
 
-app.put('/campgrounds/:id', async (req, res) => {
-  const { id } = req.params
-  const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
-  res.redirect(`/campgrounds/${campground._id}`)
-})
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
 
-
-
-
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  console.log("Campground Deleted")
+  res.redirect('/campgrounds');
+});
 
 app.listen(8080, () => {
   console.log("Listening on Port 8080");
