d44f418c0d55b1239973e4afda1871a78416fd27
nmehanma <nmehanma@gmail.com>
2021-12-30 23:20:57 -0500

    campground delete middleware introduced

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -7,10 +8,17 @@ const CampgroundSchema = new Schema({
   price: Number,
   description: String,
   location: String,
-  reviews: [
-    {type: Schema.Types.ObjectId,
-    ref: 'Review'}
-  ]
+  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }]
 });
 
-module.exports = mongoose.model('Campground', CampgroundSchema);
+CampgroundSchema.post("findOneAndDelete", async function(doc) {
+  if (doc) {
+    await Review.deleteOne({
+      _id: {
+        $in: doc.reviews
+      }
+    });
+  }
+});
+
+module.exports = mongoose.model("Campground", CampgroundSchema);
