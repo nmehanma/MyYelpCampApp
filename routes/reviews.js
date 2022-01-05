a090a7372bb5dff672ec4708d460352e9ae2706b
nmehanma <nmehanma@gmail.com>
2022-01-05 17:13:10 -0500

    reviews authorization for client and server side

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,7 +4,11 @@ const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 
-const { validateReview, isLoggedIn } = require("../middleware.js");
+const {
+  validateReview,
+  isLoggedIn,
+  isReviewAuthor
+} = require("../middleware.js");
 
 router.post(
   "/",
@@ -25,6 +29,8 @@ router.post(
 
 router.delete(
   "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
   catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
