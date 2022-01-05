10accb8a8cdb96461b233c51b6acd45c868f8eda
nmehanma <nmehanma@gmail.com>
2022-01-04 19:36:28 -0500

    Adding a Logout

--- routes/users.js
+++ routes/users.js
@@ -34,10 +34,17 @@ router.post(
   passport.authenticate("local", {
     failureFlash: true,
     failureRedirect: "/login"
-  }), (req, res) => {
+  }),
+  (req, res) => {
     req.flash("success", "Welcome back!");
     res.redirect("/campgrounds");
   }
 );
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success", "Goodbye!");
+  res.redirect("/campgrounds");
+});
 
 module.exports = router;
