1c611888f4d25bf823b6ef01d92e16d970bc644c
nmehanma <nmehanma@gmail.com>
2022-01-04 20:11:31 -0500

    ReturnTo Behaviour introduced for when user not logged in will redirect to page initially intrested in rendering after authenticated

--- middleware.js
+++ middleware.js
@@ -1,6 +1,9 @@
 module.exports.isLoggedIn = (req,res, next) => {
   console.log("REQ.USER...", req.user);
   if(!req.isAuthenticated()) {
+    //store the url they are requesting!
+    req.session.returnTo = req.originalUrl
+    console.log(req.path, req.originalUrl)
     req.flash('error', 'You must be signed in first');
     return res.redirect('/login');
   }
