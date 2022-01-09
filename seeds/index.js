28c5367ac25fd950a1eb415db4b43ca32fe094e3
nmehanma <nmehanma@gmail.com>
2022-01-09 00:30:39 -0500

    re seed data fixing our bugs for new additions made regarding coordinates

--- seeds/index.js
+++ seeds/index.js
@@ -22,6 +22,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      //YOUR USERID
       author: "61d4aee355617cdcfa633c6a",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
@@ -40,7 +41,8 @@ const seedDB = async () => {
       // image: "https://source.unsplash.com/collection/483251",
       description:
         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus qui at fugit dolorem rem. Nulla aspernatur asperiores, fugit placeat velit omnis doloribus est quam similique harum praesentium iusto fugiat minus.",
-      price
+      price,
+       geometry: { type: 'Point', coordinates: [ -79.383935, 43.653482 ] }
     });
     await camp.save();
   }
